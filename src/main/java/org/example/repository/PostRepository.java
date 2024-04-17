aee1e9874ebb7fde444f020074f72452a81b3910
Ilya Kamyshov <kamyshov@gmail.com>
2024-04-17 15:54:51 +0300

    Initial commit

---
+++ src/main/java/org/example/repository/PostRepository.java
@@ -0,0 +1,53 @@
+package org.example.repository;
+
+
+import org.example.model.Post;
+import org.example.exception.NotFoundException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+// Stub
+public class PostRepository {
+
+    private final ConcurrentHashMap<Long, Post> posts = new ConcurrentHashMap<>();
+    private final AtomicLong idCounter = new AtomicLong(0);
+
+    public List<Post> all() {
+        return new ArrayList<>(posts.values());
+    }
+
+    public Optional<Post> getById(long id) {
+        return Optional.ofNullable(posts.get(id));
+    }
+
+    public Post save(Post post) {
+        long postId = post.getId();
+
+        if (postId == 0) {
+            long newPostId = idCounter.getAndIncrement();
+            post.setId(newPostId);
+            posts.put(newPostId, post);
+        }
+
+        if (postId != 0 && !posts.containsKey(postId)) {
+            posts.put(postId, post);
+        } else {
+            throw new NotFoundException("id already exists");
+        }
+
+        return post;
+    }
+
+    public void removeById(long id) {
+        if (posts.containsKey(id)){
+            posts.remove(id);
+        }else {
+            throw new NotFoundException("id does not exist");
+        }
+    }
+}
+
---
+++ src/main/java/org/example/servlet/MainServlet.java
@@ -0,0 +1,64 @@
+package org.example.servlet;
+
+import org.example.controller.PostController;
+import org.example.repository.PostRepository;
+import org.example.service.PostService;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MainServlet extends HttpServlet {
+    private PostController controller;
+    private final static String METHOD_GET = "GET";
+    private final static String METHOD_POST = "POST";
+    private final static String METHOD_DELETE = "DELETE";
+    public static final String API_POSTS = "/api/posts";
+    public static final String API_POSTS_ID = "/api/posts/\\d+";
+
+    @Override
+    public void init() {
+        final var repository = new PostRepository();
+        final var service = new PostService(repository);
+        controller = new PostController(service);
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) {
+        // если деплоились в root context, то достаточно этого
+        try {
+            final var path = req.getRequestURI();
+            final var method = req.getMethod();
+            // primitive routing
+            if (method.equals(METHOD_GET) && path.equals(API_POSTS)) {
+                controller.all(resp);
+                return;
+            }
+            if (method.equals(METHOD_GET) && path.matches(API_POSTS_ID)) {
+                // easy way
+                final var id = getId(path);
+                controller.getById(id, resp);
+                return;
+            }
+            if (method.equals(METHOD_POST) && path.equals(API_POSTS)) {
+                controller.save(req.getReader(), resp);
+                return;
+            }
+            if (method.equals(METHOD_DELETE) && path.matches(API_POSTS_ID)) {
+                // easy way
+                final var id = getId(path);
+                controller.removeById(id, resp);
+                return;
+            }
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        } catch (Exception e) {
+            e.printStackTrace();
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    private Long getId(String path) {
+        return Long.parseLong(path.substring(path.lastIndexOf("/") + 1));
+    }
+}
+
